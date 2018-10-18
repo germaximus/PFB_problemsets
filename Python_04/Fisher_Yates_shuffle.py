a86650dac4cbc37b6c1d757143d0fea24c444def
mge <mge@pfb09.cshl.edu>
2018-10-17 18:45:56 -0700

    Fisher Yates shuffle

--- Python_04/Fisher_Yates_shuffle.py
+++ Python_04/Fisher_Yates_shuffle.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 #Fisher-Yates shuffle
 
-import sys
+import random
 
-sequence = sys.argv[1]
+sequence = input('Enter DNA sequence ')
+sequence = list(sequence)
+length = len(sequence)
+
+for i in range(length):
+  position_A = random.randrange(length)
+  position_B = random.randrange(length)
+  nt_at_posA = sequence[position_A]
+  sequence[position_A] = sequence[position_B]
+  sequence[position_B] = nt_at_posA
+  print('Step', i+1,''.join(sequence))
+
+
+
+
+
 
 
 
